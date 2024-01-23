559760b2873e1c8b326d2ffc5d8c53c502a4f223
Tristan Mandery <tlmandery@mavs.coloradomesa.edu>
2024-01-23 11:51:49 -0700

    Our First Hello World Program

---
+++ hello.cpp
@@ -0,0 +1,15 @@
+/*
+Name: Tristan Mandery
+
+First Hello World Program
+*/
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    //This will print out hello world to the console 
+    cout << "Hello World" <<endl;
+    return 0; // This exits the program with the value 0
+}
