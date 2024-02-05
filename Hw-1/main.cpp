a6fd4cdcca297ca5834507b6970c68d6b0020085
Tristan Mandery <tlmandery@mavs.coloradomesa.edu>
2024-02-05 09:39:46 -0700

    New changes

--- Hw-1/main.cpp
+++ Hw-1/main.cpp
@@ -3,12 +3,20 @@ Name: Tristan Mandery
 
 Hangman Game
 */
+
 #include <iostream>
 
 using namespace std;
 
 int main()
 {
+    string somePhrase;
+    somePhrase = "Hey there, what is your name?"; 
+    cout << somePhrase << endl;
+
+    
 
 
+
+    return 0;
 }
